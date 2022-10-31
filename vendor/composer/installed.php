6dcf5f25cc78b38fa9f5de5b0747107ebc086741
hai2k1 <trihai306201@gmail.com>
2022-10-31 11:11:41 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'haibase/hairavel-static',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '51db62a1c51f8d255049bf92cde81b226f38380c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'haibase/hairavel-static' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '51db62a1c51f8d255049bf92cde81b226f38380c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
