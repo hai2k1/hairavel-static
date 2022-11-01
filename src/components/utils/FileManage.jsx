6c8fe09d73ebf37d372a79efdca90f9f6169fa31
hai2k1 <trihai306201@gmail.com>
2022-11-01 17:53:37 +0700

    update

--- src/components/utils/FileManage.jsx
+++ src/components/utils/FileManage.jsx
@@ -266,7 +266,6 @@ export default defineComponent({
 
             <div class="arco-modal-header flex gap-2">
                 <div class="flex-grow flex flex-row gap-2">
-                    {!!this.filter.id &&
                         <a-upload
                             action={getUrl(this.api.upload)}
                             accept={this.accept}
@@ -281,19 +280,19 @@ export default defineComponent({
                             multiple
                             showFileList={false}
                         >
-                            {{
-                                'upload-button': () => <div class="text-gray-600 dark:text-gray-400 absolute flex items-center justify-center w-full h-full bg-gray-100 hover:bg-gray-200 dark:bg-blackgray-1 dark:hover:bg-blackgray-2 rounded cursor-pointer text-center">
-                                    {this.progress.status ?
-                                        <div class="text-xl"> {this.progress.progress}%</div> :
-                                        <div class="flex items-center flex-col justify-center ">
-                                            <icon-upload class="text-2xl"/>
-                                            <div class="mt-2">upload image</div>
-                                        </div>}
-                                </div>
-                            }}
-                            {this.uploadProgress.status ? this.uploadProgress.progress + '%' : 'upload files'}
-
-                        </a-upload>}
+                            {
+                                {
+                                    'upload-button': () => <div class="text-gray-600 dark:text-gray-400 absolute flex items-center justify-center w-full h-full bg-gray-100 hover:bg-gray-200 dark:bg-blackgray-1 dark:hover:bg-blackgray-2 rounded cursor-pointer text-center">
+                                        {this.progress.status ?
+                                            <div class="text-xl"> {this.progress.progress}%</div> :
+                                            <div class="flex items-center flex-col justify-center ">
+                                                <icon-upload class="text-2xl"/>
+                                                <div class="mt-2">upload image</div>
+                                            </div>}
+                                    </div>
+                                }
+                            }
+                        </a-upload>
                 </div>
                 <div class="flex-none flex flex-row gap-2">
                     {this.typeOption.length > 1 && <div class="w-32">
